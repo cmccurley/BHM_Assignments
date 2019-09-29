be2f0bbfcde1954c36f915f6cb1451026018d0b4
Connor <cmccurley@ufl.edu>
2019-09-29 12:47:15 -0400

    Finished code for HW02

--- HW02/Code/hw02.py
+++ HW02/Code/hw02.py
@@ -26,55 +26,17 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
-import pandas as pd
 import math
 
 
 ######################################################################
 ######################## Funciton Definitions ########################
 ######################################################################
-def find_convergence_in_dist(numSamples, distParameters, dist):
-    
-    simData = dict()
-    
-    sim = 1
-    numSims = len(distParameters)  
-    
-    for p in distParameters:
-        dataStruct = dict()
-        
-        # update status of simulations
-        print(f'Simulation {sim} of {numSims}')
-        p_mean_est = []
-        p_var_est = []
-        pVal = p
-        
-        # get mean estimate and variance of mean estimate for a range of sample values, 100 trials each.
-        for n in numSamples:
-            if(dist=='binomial'):
-                samples = np.random.binomial(n,p,size=(100,n))
-                mean_est = np.mean(samples, axis=1)
-                var_of_mean_est = np.var(mean_est)
-                mean_of_mean_est = np.mean(mean_est)
-                
-#            elif(dist=='poisson'):
-                p_mean_est.append(mean_of_mean_est)
-                p_var_est.append(var_of_mean_est)
-        
-        # add simulation over n to structure of all trials
-        key = 'sim' + str(sim)
-        dataStruct["p_mean_est"] = p_mean_est
-        dataStruct["p_var_est"] = p_var_est
-        dataStruct["pVal"] = pVal
-        simData[key] = dataStruct
-        sim = sim + 1
-        
-    return dataStruct
 
 """
 ***********************************************************************
     *  Func:  bin_convergence_to_normal()
-    *  Desc:  
+    *  Desc:  Demonstrate convergence of Binomial to the Normal distribution
 **********************************************************************
 """
 def bin_convergence_to_normal(n, p):
@@ -109,7 +71,7 @@ def bin_convergence_to_normal(n, p):
 """
 ***********************************************************************
     *  Func:  poisson_convergence_to_normal()
-    *  Desc:  
+    *  Desc:  Demonstrate convergence of Poisson to the Normal distribution
 **********************************************************************
 """
 def poisson_convergence_to_normal(n, lam):
@@ -145,7 +107,7 @@ def poisson_convergence_to_normal(n, lam):
 """
 ***********************************************************************
     *  Func:  plot_gamma()
-    *  Desc:  
+    *  Desc:  Plot the Gamma distribution for vaious shape parameters
 **********************************************************************
 """
 def plot_gamma(alpha, beta):
@@ -175,7 +137,7 @@ def plot_gamma(alpha, beta):
     *  Desc:  
 **********************************************************************
 """
-def binomial_monte_carlo(p, numFlips):
+def binomial_monte_carlo_single_trial(p, numFlips):
     
     simData = dict()
     simData["p"] = p
@@ -187,11 +149,25 @@ def binomial_monte_carlo(p, numFlips):
     
     return simData
 
+"""
+***********************************************************************
+    *  Func:  binomial_monte_carlo_single_trial_size()
+    *  Desc:  Generate samples from a Binomial for a single MC simulation
+**********************************************************************
+"""
+def binomial_monte_carlo(p, n):
+    
+    print(f'Generating data for p={p}, n={n}')
+    simData = np.random.binomial(n, p, size = (100, 1000000)) 
+    
+    
+    return simData
+
 
 """
 ***********************************************************************
     *  Func:  est_prop_heads()
-    *  Desc:  
+    *  Desc:  Return the proportion of heads from Binomial samples 
 **********************************************************************
 """
 def est_prop_heads(simData, numFlips):
@@ -201,24 +177,18 @@ def est_prop_heads(simData, numFlips):
         
         # calculate proportions of heads
         propVec = data/n 
-        
-        # get mean of proportions
-        meanPropVec = np.mean(propVec)
-        
-        
-        # get deviation within 95%
     
-    return 
+    return propVec
 
 
 """
 ***********************************************************************
     *  Func:  estimate_p_val()
-    *  Desc:  
+    *  Desc:  Plot histogram of estimated p values from Binomial Distribution
+    *         using MLE
 **********************************************************************
 """
 def estimate_p_val(simData, numFlips):
-    mle_est = dict()
     
     true_p_val = simData["p"]
     
@@ -236,7 +206,193 @@ def estimate_p_val(simData, numFlips):
         plt.title(f'Histogram of Binomial Trials \n Number of Flips={n} \n True p Value={true_p_val} \n MLE Estimate={mle_p_est}')
         
         
-    return mle_p_est
+    return
+
+
+"""
+***********************************************************************
+    *  Func:  compute_point_est_std_error()
+    *  Desc:  Find the point estimate standard deviation from a single
+    *         MC trial
+**********************************************************************
+"""
+def compute_point_est_std_error(simData, numFlips):
+    
+    point_error = dict()
+    
+    # generate 100 MC simulations with 1000000 samples each
+    for n in numFlips: 
+        p = simData["p"]
+        data = simData[str(n)]
+        data = data/n
+#        mean_est = np.mean(data)
+        N = data.shape[1]
+        s_y_squared = (1/(N-1))*np.sum((data-p)**2)
+        
+        point_error[str(n)] = np.sqrt(s_y_squared)
+    
+    return point_error
+
+"""
+***********************************************************************
+    *  Func:  compute_mc_std_error()
+    *  Desc:  Find the mean and variance of parameter estimate error
+    *         for each of the MC simulations
+**********************************************************************
+"""
+def compute_mc_std_error(p, numFlips):
+    
+    estData = dict()
+    
+    # generate 100 MC simulations with 1000000 samples each
+    for n in numFlips:    
+        data = binomial_monte_carlo(p, n)
+        data = data/n
+        
+        # calculate statistics of estimate error
+        mean_est = np.mean(data, axis=1) - p
+        mean_of_mean_est = np.mean(mean_est-p)
+        std_of_mean_est = np.std(mean_est-p)
+        
+        # save data
+        n_summary = dict()
+        n_summary["mean_est"] = mean_est
+        n_summary["mean_of_mean_est"] = mean_of_mean_est
+        n_summary["std_of_mean_est"] = std_of_mean_est
+    
+        estData[str(n)] = n_summary
+        
+    return estData
+
+"""
+***********************************************************************
+    *  Func:  plot_error_hist()
+    *  Desc:  Plot histogram of MC errors 
+**********************************************************************
+"""
+def plot_error_hist(data):
+    
+    # plot histogram of error for the mean estimates
+    for n in data.keys():    
+        
+        mean_est = data[str(n)]["mean_est"]
+        mean_of_mean_est = data[str(n)]["mean_of_mean_est"]
+        std_of_mean_est = data[str(n)]["std_of_mean_est"]
+        
+        # plot histogram
+        plt.figure()
+        plt.hist(mean_est, bins='auto', density=True)
+        plt.title(f'Histogram of Estimate Errors for 100 MC Simulations \n Number of Flips={n} \n Mean={mean_of_mean_est:.2f}, STD={std_of_mean_est:.5f}')
+        
+    return
+
+"""
+***********************************************************************
+    *  Func:  plot_Q6_trajectories()
+    *  Desc:  Plot dN/dt for various combinations of r and K
+**********************************************************************
+"""
+def plot_Q6_trajectories(rList, KList):
+    
+    plt.figure()
+    N = np.linspace(0,15,100)
+    leg = []
+    
+    for r in rList:
+        for K in KList:
+            
+            # evaluate differential equation
+            y = (r*N)*(1-(N/K))
+        
+            # plot trajectory
+            plt.plot(N, y)
+            legEntry = 'r= ' + str(r) + ', K= ' + str(K)
+            leg.append(legEntry)
+    plt.legend(leg)
+    plt.title('Trajectories for dN/dt', fontsize=16)
+    plt.xlabel('N', fontsize=14)
+    plt.ylabel('dN/dt', fontsize=14)
+    plt.ylim(-5,30)
+    
+    return
+
+"""
+***********************************************************************
+    *  Func:  Q6_observation_error()
+    *  Desc:  Generate plot of 10**3 sample paths with observation error
+**********************************************************************
+"""
+def Q6_observation_error(r, K):
+    
+    numSamples = 200  # length of sample path
+    numTrials = 10000 # number of sample paths
+    n = np.zeros((numTrials, numSamples))
+    n[:,0] = 0.5
+    tRange = np.arange(0,numSamples)
+    
+    
+    plt.figure()
+    
+    for trial in range(0, numTrials):
+        # print status
+        if not(trial % 1000):
+            print(f'Trial {trial} of {numTrials}')
+            
+        # generate sample path N[t] = N[t-1] + r*N[t-1]*(1-(N[t-1]/K)) + M
+        for t in range(1, numSamples):
+            # generate process sample
+            n[trial, t] = (n[trial,t-1] + (r*n[trial, t-1])*(1-(n[trial,t-1]/K)))
+            
+        # add observation error
+        n[trial, :] = n[trial, :] + np.random.normal(0,0.5, size=(1,numSamples))
+            
+        plt.plot(tRange, n[trial, :], color='black')
+        
+    # plot sample paths
+    plt.xlim(0,numSamples)
+    plt.ylim(-5,10)
+    
+    plt.title(f'{numTrials} Sample Paths with Observation Error')
+    
+    return
+
+"""
+***********************************************************************
+    *  Func:  Q6_process_error()
+    *  Desc:  Generate plot of 10**3 sample paths with process error
+**********************************************************************
+"""
+def Q6_process_error(r, K):
+    
+    numSamples = 200  # length of sample path
+    numTrials = 10000 # number of sample paths
+    n = np.zeros((numTrials, numSamples))
+    n[:,0] = 0.5
+    tRange = np.arange(0,numSamples)
+    
+    
+    plt.figure()
+    
+    for trial in range(0, numTrials):
+        # print status
+        if not(trial % 1000):
+            print(f'Trial {trial} of {numTrials}')
+            
+        # generate sample path N[t] = N[t-1] + r*N[t-1]*(1-(N[t-1]/K)) + M
+        for t in range(1, numSamples):
+            
+            # generate sample with process error
+            n[trial, t] = (n[trial,t-1] + (r*n[trial, t-1])*(1-(n[trial,t-1]/K))) + np.random.normal(0,0.5)
+        
+        plt.plot(tRange, n[trial, :], color='black')
+        
+    # plot sample paths
+    plt.xlim(0,numSamples)
+    plt.ylim(-5,10)
+    
+    plt.title(f'{numTrials} Sample Paths with Process Error')
+    
+    return
 
 ######################################################################
 ############################ Questions ###############################
@@ -277,29 +433,54 @@ if __name__== "__main__":
     numFlips = [5, 10, 50, 100]
     
     # generate data
-#    simData = binomial_monte_carlo(p, numFlips)
+#    simData = binomial_monte_carlo_single_trial(p, numFlips)
 #    np.save('Q4_data.npy', simData, allow_pickle=True)
     
     # load data
-    simData = np.load('Q4_data.npy', allow_pickle=True).item()
+#    simData = np.load('Q4_data.npy', allow_pickle=True).item()
     
     # estimate bounds for expected value of each trial
 #    est_prop_heads(simData, numFlips)
+    
+    # compute standard errors 
+#    point_error = compute_point_est_std_error(simData, numFlips)
+        
     
     
     ############################ Question 5 ##############################
     # compute MLE estimate for p parameter of binomial
     
     # load data
-    simData = np.load('Q4_data.npy', allow_pickle=True).item()
+#    simData = np.load('Q4_data.npy', allow_pickle=True).item()
     
     
     # compute the MLE estimates
-    mle_est = estimate_p_val(simData, numFlips)
+#    estimate_p_val(simData, numFlips)
+    
+    # compute Monte Carlo standard errors
+#    error_data = compute_mc_std_error(p, numFlips)
+    
+    # plot error histograms
+#    plot_error_hist(error_data)
     
     ############################ Question 6 ##############################
     
+    # plot simulated trajectories for r and K
+    r = [0.1, 1, 5, 10]
+    K = [0.1, 1, 5, 10]
     
+#    plot_Q6_trajectories(r, K)
+    
+    
+    # descritize the model
+    r = 0.1
+    K = 1
+    
+    # plot trajectories with observation error
+#    Q6_observation_error(r, K)
+    
+    # plot trajectories with process error
+#    Q6_process_error(r, K)
     
     
     
